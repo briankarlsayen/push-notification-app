90c3ed91e5bf87ed324da92a6b5d90cfdbf2041d
BrianSayen <blu3fire89@gmail.com>
2022-04-25 10:28:28 +0800

    update: apply injectManifest

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({})]
+  plugins: [
+    react(), 
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js'
+    })]
 })
